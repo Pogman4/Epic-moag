0f8556e3be08e465e5696f17e4cbd59071d53f2c
ostegamer <ostegamer@users.noreply.github.com>
2021-11-08 12:26:32 +0100

    EBIC

--- epic moag game.py
+++ epic moag game.py
@@ -1,4 +1,14 @@
-print("anyone sus")
-answer = input()
-print(answer + " " "was the Impostor")
-print("YOU WIN")
+import random
+b = 0
+while b < 3:
+ a = random.randint(1,2)
+ print("anyone sus")
+ answer = input()
+ if (a == 1):
+    print(answer + " " "was the Impostor")
+    print("YOU WIN")
+    b = 3
+ if (a == 2):
+    print(answer + " " "was not the Impostor")
+    b + 1
+    a=0
