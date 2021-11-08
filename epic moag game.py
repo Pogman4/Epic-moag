8023de78df52c0497c864b12afc0a788752fd450
ostegamer <ostegamer@users.noreply.github.com>
2021-11-08 12:34:00 +0100

    Better mogus
    Ratio + YB better

--- epic moag game.py
+++ epic moag game.py
@@ -1,14 +1,16 @@
 import random
 b = 0
 while b < 3:
- a = random.randint(1,2)
+ a = random.randint(1,3)
  print("anyone sus")
  answer = input()
  if (a == 1):
     print(answer + " " "was the Impostor")
     print("YOU WIN")
-    b = 3
- if (a == 2):
+    b = 10
+ if (a > 1):
     print(answer + " " "was not the Impostor")
-    b + 1
+    b = b+1
     a=0
+ if (b == 3):
+    print("you lost")
